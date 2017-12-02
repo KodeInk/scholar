0ec8409df2c6e78de5d22ede177c0b7ac57ed54e
Muyinda Rogers <moverr@gmail.com>
2017-12-02 08:15:59 +0300

    SubjectGrading Jpa

---
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/db/controllers/SubjectGradingJpaController.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.engine.db.controllers;
+
+import com.codemovers.scholar.engine.db.JpaController;
+import com.codemovers.scholar.engine.db.entities.StudyYearCurriculum;
+import com.codemovers.scholar.engine.db.entities.SubjectCurriculum;
+import com.codemovers.scholar.engine.db.entities.SubjectGrading;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.ws.rs.BadRequestException;
+
+/**
+ *
+ * @author Manny
+ */
+public class SubjectGradingJpaController extends JpaController {
+
+    protected static final Logger LOG = Logger.getLogger(SubjectGradingJpaController.class.getName());
+
+    private static SubjectGradingJpaController controller = null;
+
+    public static SubjectGradingJpaController getInstance() {
+        if (controller == null) {
+            controller = new SubjectGradingJpaController();
+        }
+        return controller;
+    }
+
+    public SubjectGradingJpaController() {
+        super(SubjectGrading.class);
+    }
+
+    public SubjectGrading create(SubjectGrading entity) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        } catch (Exception eml) {
+            LOG.log(Level.INFO, eml.toString());
+            throw eml;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+        return entity;
+
+    }
+
+    public void edit(SubjectGrading subjectGrading) throws Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            subjectGrading = em.merge(subjectGrading);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = subjectGrading.getId().intValue();
+                if (findSubjectGrading(id) == null) {
+                    throw new BadRequestException("The Inventory with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public SubjectGrading findSubjectGrading(Integer id) {
+        EntityManager em = getEntityManager();
+
+        try {
+            return em.find(SubjectGrading.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    private List<SubjectGrading> findSubjectGradings(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(StudyYearCurriculum.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<SubjectGrading> findSubjectGradings() {
+        return findSubjectGradings(true, -1, -1);
+    }
+
+    public List<SubjectGrading> findSubjectGradings(int maxResults, int firstResult) {
+        return findSubjectGradings(false, maxResults, firstResult);
+    }
+
+    public int getCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<SubjectGrading> rt = cq.from(SubjectGrading.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return (Integer) q.getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
+
+}
