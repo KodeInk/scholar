725b437fb4f12de9cf4c532fbf4bf7e61059da6d
Muyinda Rogers <moverr@gmail.com>
2017-12-02 10:04:01 +0300

    Users Jpa Controller

---
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/db/controllers/UsersJpaController.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.engine.db.controllers;
+
+import com.codemovers.scholar.engine.db.JpaController;
+import com.codemovers.scholar.engine.db.entities.Terms;
+import com.codemovers.scholar.engine.db.entities.Users;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.ws.rs.BadRequestException;
+
+/**
+ *
+ * @author Manny
+ */
+public class UsersJpaController extends JpaController {
+
+    protected static final Logger LOG = Logger.getLogger(UsersJpaController.class.getName());
+
+    private static UsersJpaController controller = null;
+
+    public static UsersJpaController getInstance() {
+        if (controller == null) {
+            controller = new UsersJpaController();
+        }
+        return controller;
+    }
+
+    public UsersJpaController() {
+        super(Users.class);
+    }
+
+    public Terms create(Users entity) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        } catch (Exception eml) {
+            LOG.log(Level.INFO, eml.toString());
+            throw eml;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+        return entity;
+
+    }
+
+    public void edit(Users user) throws Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            user = em.merge(user);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = user.getId().intValue();
+                if (findUser(id) == null) {
+                    throw new BadRequestException("The Inventory with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public Users findUser(Integer id) {
+        EntityManager em = getEntityManager();
+
+        try {
+            return em.find(Users.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    private List<Users> findUsers(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Terms.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Users> findUsers() {
+        return findUsers(true, -1, -1);
+    }
+
+    public List<Users> findUsers(int maxResults, int firstResult) {
+        return findUsers(false, maxResults, firstResult);
+    }
+
+    public int getCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Users> rt = cq.from(Users.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return (Integer) q.getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
+
+}
