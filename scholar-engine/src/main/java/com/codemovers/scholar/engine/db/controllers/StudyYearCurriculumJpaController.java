0f5f44316cb93dc34ad7fd978aa02271279ec5aa
Muyinda Rogers <moverr@gmail.com>
2017-12-02 08:07:30 +0300

    StudyYearCurriculum Jpa Controller

---
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/db/controllers/StudyYearCurriculumJpaController.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.engine.db.controllers;
+
+import com.codemovers.scholar.engine.db.JpaController;
+import com.codemovers.scholar.engine.db.entities.StudyYear;
+import com.codemovers.scholar.engine.db.entities.StudyYearCurriculum;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.ws.rs.BadRequestException;
+
+/**
+ *
+ * @author Manny
+ */
+public class StudyYearCurriculumJpaController extends JpaController {
+
+    protected static final Logger LOG = Logger.getLogger(StudyYearCurriculumJpaController.class.getName());
+
+    private static StudyYearCurriculumJpaController controller = null;
+
+    public static StudyYearCurriculumJpaController getInstance() {
+        if (controller == null) {
+            controller = new StudyYearCurriculumJpaController();
+        }
+        return controller;
+    }
+
+    public StudyYearCurriculumJpaController() {
+        super(StudyYearCurriculum.class);
+    }
+
+    public StudyYearCurriculum create(StudyYearCurriculum entity) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        } catch (Exception eml) {
+            LOG.log(Level.INFO, eml.toString());
+            throw eml;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+        return entity;
+
+    }
+
+    public void edit(StudyYearCurriculum studyYearCurriculum) throws Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            studyYearCurriculum = em.merge(studyYearCurriculum);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = studyYearCurriculum.getId().intValue();
+                if (findStudyYearCurriculum(id) == null) {
+                    throw new BadRequestException("The Inventory with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public StudyYearCurriculum findStudyYearCurriculum(Integer id) {
+        EntityManager em = getEntityManager();
+
+        try {
+            return em.find(StudyYearCurriculum.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    private List<StudyYearCurriculum> findStudyYearCurriculums(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(StudyYearCurriculum.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<StudyYearCurriculum> findStudyYearCurriculums() {
+        return findStudyYearCurriculums(true, -1, -1);
+    }
+
+    public List<StudyYearCurriculum> findStudyYearCurriculums(int maxResults, int firstResult) {
+        return findStudyYearCurriculums(false, maxResults, firstResult);
+    }
+
+    public int getCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<StudyYearCurriculum> rt = cq.from(StudyYearCurriculum.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return (Integer) q.getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
+
+}
