d6ed66e55a63accad0d14c717b968aa54783625f
Muyinda Rogers <moverr@gmail.com>
2017-12-02 07:44:07 +0300

    Roles Jpa Controller

---
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/db/controllers/ProfileJpaController.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.engine.db.controllers;
+
+import com.codemovers.scholar.engine.db.JpaController;
+import com.codemovers.scholar.engine.db.entities.Marksheet;
+import com.codemovers.scholar.engine.db.entities.Profile;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.ws.rs.BadRequestException;
+
+/**
+ *
+ * @author Manny
+ */
+public class ProfileJpaController extends JpaController {
+
+    protected static final Logger LOG = Logger.getLogger(MarksheetJpaController.class.getName());
+
+    private static ProfileJpaController controller = null;
+
+    public static ProfileJpaController getInstance() {
+        if (controller == null) {
+            controller = new ProfileJpaController();
+        }
+        return controller;
+    }
+
+    public ProfileJpaController() {
+        super(Profile.class);
+    }
+
+    public Profile create(Profile entity) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        } catch (Exception eml) {
+            LOG.log(Level.INFO, eml.toString());
+            throw eml;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+        return entity;
+
+    }
+
+    public void edit(Profile profile) throws Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            profile = em.merge(profile);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = profile.getId().intValue();
+                if (findProfile(id) == null) {
+                    throw new BadRequestException("The Inventory with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public Profile findProfile(Integer id) {
+        EntityManager em = getEntityManager();
+
+        try {
+            return em.find(Profile.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    private List<Profile> findProfiles(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Profile.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Profile> findProfiles() {
+        return findProfiles(true, -1, -1);
+    }
+
+    public List<Profile> findProfiles(int maxResults, int firstResult) {
+        return findProfiles(false, maxResults, firstResult);
+    }
+
+    public int getCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Profile> rt = cq.from(Profile.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return (Integer) q.getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
+
+}
---
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/db/controllers/RolesJpaController.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.engine.db.controllers;
+
+import com.codemovers.scholar.engine.db.JpaController;
+import com.codemovers.scholar.engine.db.entities.Profile;
+import com.codemovers.scholar.engine.db.entities.Roles;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.ws.rs.BadRequestException;
+
+/**
+ *
+ * @author Manny
+ */
+public class RolesJpaController extends JpaController {
+
+    protected static final Logger LOG = Logger.getLogger(RolesJpaController.class.getName());
+
+    private static RolesJpaController controller = null;
+
+    public static RolesJpaController getInstance() {
+        if (controller == null) {
+            controller = new RolesJpaController();
+        }
+        return controller;
+    }
+
+    public RolesJpaController() {
+        super(Roles.class);
+    }
+
+    public Roles create(Roles entity) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        } catch (Exception eml) {
+            LOG.log(Level.INFO, eml.toString());
+            throw eml;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+        return entity;
+
+    }
+
+    public void edit(Roles role) throws Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            role = em.merge(role);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = role.getId().intValue();
+                if (findRole(id) == null) {
+                    throw new BadRequestException("The Inventory with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public Roles findRole(Integer id) {
+        EntityManager em = getEntityManager();
+
+        try {
+            return em.find(Roles.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    private List<Roles> findRoles(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Roles.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Roles> findRoles() {
+        return findRoles(true, -1, -1);
+    }
+
+    public List<Roles> findRoles(int maxResults, int firstResult) {
+        return findRoles(false, maxResults, firstResult);
+    }
+
+    public int getCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Roles> rt = cq.from(Roles.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return (Integer) q.getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
+
+}
