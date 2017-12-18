a6e6f9bde559f2dccdd55fb98ff4d99e6b7bd455
Muyinda Rogers <muyinda@awamo.com>
2017-12-18 17:22:22 +0300

    User

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/users/UsersEndpoint.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/users/UsersEndpoint.java
@@ -6,13 +6,20 @@
 package com.codemovers.scholar.engine.api.v1.users;
 
 import com.codemovers.scholar.engine.api.v1.abstracts.AbstractEndpoint;
+import com.codemovers.scholar.engine.api.v1.accounts.AccountsService;
+import com.codemovers.scholar.engine.api.v1.accounts.entities.AuthenticationResponse;
+import com.codemovers.scholar.engine.api.v1.accounts.entities._login;
 import com.codemovers.scholar.engine.api.v1.users.entities.UserResponse;
 import com.codemovers.scholar.engine.api.v1.users.entities._User;
+import static com.codemovers.scholar.engine.helper.Utilities.tenantdata;
 import java.util.Collection;
+import java.util.logging.Level;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -22,6 +29,12 @@ import javax.ws.rs.core.Response;
  */
 @Path("/")
 public class UsersEndpoint extends AbstractEndpoint<_User, UserResponse> {
+
+    private AccountsService service = null;
+
+    public UsersEndpoint() {
+        service = new AccountsService();
+    }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
@@ -48,6 +61,24 @@ public class UsersEndpoint extends AbstractEndpoint<_User, UserResponse> {
     @Override
     public Collection<UserResponse> list(int start, int end) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @POST
+    @Path("login/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public AuthenticationResponse login(
+            _login login,
+            @Context HttpServletRequest httpRequest
+    ) throws Exception {
+        try {
+            String logId = context.getProperty("logId").toString();
+            LOG.log(Level.INFO, " IF THIS WORKS {0} CELEBERATION ", tenantdata.getExternalId());
+            return service.login(tenantdata, login, logId);
+        } catch (Exception er) {
+            throw er;
+        }
+
     }
 
 }
