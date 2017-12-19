6000a22cc3daebef77871511be83d0f7211223e3
Muyinda Rogers <moverr@gmail.com>
2017-12-19 21:20:26 +0300

    Stream Pojo

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/streams/entities/_Stream.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/streams/entities/_Stream.java
@@ -5,19 +5,141 @@
  */
 package com.codemovers.scholar.engine.api.v1.streams.entities;
 
+import com.codemovers.scholar.engine.api.v1.abstracts.AbstractEntity;
+import static com.codemovers.scholar.engine.helper.Utilities.validateMandatoryFields;
 import com.codemovers.scholar.engine.helper.enums.StatusEnum;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  *
  * @author mover
  */
-public class _Stream {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class _Stream extends AbstractEntity {
 
     private Integer id;
     private String name;
     private String code;
     private StatusEnum status;
     private Date date_created;
+    private Integer author_id;
+
+    public _Stream() {
+    }
+
+    public _Stream(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+
+    public Integer getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(Integer author_id) {
+        this.author_id = author_id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        hash = 53 * hash + Objects.hashCode(this.name);
+        hash = 53 * hash + Objects.hashCode(this.code);
+        hash = 53 * hash + Objects.hashCode(this.status);
+        hash = 53 * hash + Objects.hashCode(this.date_created);
+        hash = 53 * hash + Objects.hashCode(this.author_id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final _Stream other = (_Stream) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.code, other.code)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (this.status != other.status) {
+            return false;
+        }
+        if (!Objects.equals(this.date_created, other.date_created)) {
+            return false;
+        }
+        return Objects.equals(this.author_id, other.author_id);
+    }
+
+
+    @Override
+    public void validate() {
+        validateMandatoryFields(this.getClass(), this);
+    }
+
+    @Override
+    public String toString() {
+        return "_Stream{"
+                + "id=" + id
+                + ", name=" + name
+                + ", code=" + code
+                + ", status=" + status
+                + ", date_created=" + date_created
+                + ", author_id=" + author_id
+                + "}";
+    }
+
 
 }
