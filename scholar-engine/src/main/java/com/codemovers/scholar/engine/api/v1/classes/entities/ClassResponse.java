4fb26ae8d7f4f4ba35c2ba573876182bec078afe
Muyinda Rogers <muyinda@awamo.com>
2017-12-19 12:44:38 +0300

    Class Response

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/classes/entities/ClassResponse.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/classes/entities/ClassResponse.java
@@ -5,10 +5,145 @@
  */
 package com.codemovers.scholar.engine.api.v1.classes.entities;
 
+import com.codemovers.scholar.engine.helper.enums.StatusEnum;
+import java.util.Date;
+import java.util.Objects;
+import java.util.logging.Logger;
+
 /**
  *
  * @author mover
  */
 public class ClassResponse {
+
+    private Integer id;
+    private String name;
+    private String code;
+    private Integer ranking;
+    private StatusEnum status;
+    private Date date_created;
+    private Integer author_id;
+
+    public ClassResponse() {
+    }
+
+    public ClassResponse(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(Integer ranking) {
+        this.ranking = ranking;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+
+    public Integer getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(Integer author_id) {
+        this.author_id = author_id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.id);
+        hash = 41 * hash + Objects.hashCode(this.name);
+        hash = 41 * hash + Objects.hashCode(this.code);
+        hash = 41 * hash + Objects.hashCode(this.ranking);
+        hash = 41 * hash + Objects.hashCode(this.status);
+        hash = 41 * hash + Objects.hashCode(this.date_created);
+        hash = 41 * hash + Objects.hashCode(this.author_id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ClassResponse other = (ClassResponse) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.code, other.code)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.ranking, other.ranking)) {
+            return false;
+        }
+        if (this.status != other.status) {
+            return false;
+        }
+        if (!Objects.equals(this.date_created, other.date_created)) {
+            return false;
+        }
+        return Objects.equals(this.author_id, other.author_id);
+    }
+
+    @Override
+    public String toString() {
+        return "ClassResponse{"
+                + "id=" + id
+                + ", name=" + name
+                + ", code=" + code
+                + ", ranking=" + ranking
+                + ", status=" + status
+                + ", date_created=" + date_created
+                + ", author_id=" + author_id
+                + "}";
+    }
 
 }
