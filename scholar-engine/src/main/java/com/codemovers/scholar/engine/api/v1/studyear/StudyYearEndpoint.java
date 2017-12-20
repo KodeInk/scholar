b1989897504df68af840a2f2dbcb2c91436f2b26
Muyinda Rogers <moverr@gmail.com>
2017-12-20 20:02:16 +0300

    Study Year Endpoint

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/studyear/StudyYearEndpoint.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/studyear/StudyYearEndpoint.java
@@ -5,10 +5,60 @@
  */
 package com.codemovers.scholar.engine.api.v1.studyear;
 
+import com.codemovers.scholar.engine.api.v1.abstracts.AbstractEndpoint;
+import com.codemovers.scholar.engine.api.v1.streams.StreamsEndpoint;
+import com.codemovers.scholar.engine.api.v1.streams.StreamsService;
+import com.codemovers.scholar.engine.api.v1.studyear.entities.StudyYearResponse;
+import com.codemovers.scholar.engine.api.v1.studyear.entities._StudyYear;
+import com.codemovers.scholar.engine.db.entities.SchoolData;
+import java.util.Collection;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
 /**
  *
- * @author Manny
+ * @author mover 12/20/2017
  */
-public class StudyYearEndpoint {
+public class StudyYearEndpoint extends AbstractEndpoint<_StudyYear, StudyYearResponse> {
+
+    private static final Logger LOG = Logger.getLogger(StudyYearEndpoint.class.getName());
+    @Context
+    private ContainerRequestContext context;
+
+    StudyYearService service = null;
+
+    public StudyYearEndpoint() {
+        service = new StudyYearService();
+    }
+
+    @Override
+    public void validateAuthentication(SchoolData schoolData, String authentication) {
+        super.validateAuthentication(schoolData, authentication); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudyYearResponse create(_StudyYear entity, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.create(entity, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudyYearResponse update(_StudyYear entity, String authentication, HttpServletRequest httpRequest) {
+        return super.update(entity, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Response archive(Integer id, String authentication, HttpServletRequest httpRequest) {
+        return super.archive(id, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<StudyYearResponse> list(int start, int end, String authentication, HttpServletRequest httpRequest) {
+        return super.list(start, end, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
 
 }
