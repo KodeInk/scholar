adb043205e49664f9651c0aa4f615fd348ea377e
Muyinda Rogers <moverr@gmail.com>
2017-12-20 19:13:51 +0300

    Subjects Service

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/subjects/SubjectService.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/subjects/SubjectService.java
@@ -5,10 +5,35 @@
  */
 package com.codemovers.scholar.engine.api.v1.subjects;
 
+import com.codemovers.scholar.engine.api.v1.abstracts.AbstractService;
+import com.codemovers.scholar.engine.api.v1.subjects.entities.SubjectResponse;
+import com.codemovers.scholar.engine.api.v1.subjects.entities._Subject;
+import com.codemovers.scholar.engine.db.controllers.SubjectsJpaController;
+import java.util.logging.Logger;
+
 /**
  *
  * @author mover 12/20/2017
  */
-public class SubjectService {
+public class SubjectService extends AbstractService<_Subject, SubjectResponse> {
+
+    private static final Logger LOG = Logger.getLogger(SubjectService.class.getName());
+
+    private final SubjectsJpaController controller;
+
+    private static SubjectService service = null;
+
+    public SubjectService() {
+        controller = SubjectsJpaController.getInstance();
+    }
+
+    public static SubjectService getInstance() {
+        if (service == null) {
+            service = new SubjectService();
+        }
+        return service;
+    }
+
+
 
 }
