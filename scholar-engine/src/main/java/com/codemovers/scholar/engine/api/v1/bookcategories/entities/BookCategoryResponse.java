69f79644138cc74350feb9419e39902b922842e7
Muyinda Rogers <moverr@gmail.com>
2017-12-20 21:46:53 +0300

    Book categories

---
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/bookcategories/entities/BookCategoryResponse.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.engine.api.v1.bookcategories.entities;
+
+import com.codemovers.scholar.engine.helper.enums.StatusEnum;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author mover 12/20/2017
+ */
+public class BookCategoryResponse {
+
+    private Integer id;
+    private    String name;
+    private StatusEnum status;
+    private Date date_created;
+    private Integer author_id;
+
+    public BookCategoryResponse() {
+    }
+
+    public BookCategoryResponse(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+
+    public Integer getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(Integer author_id) {
+        this.author_id = author_id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        hash = 89 * hash + Objects.hashCode(this.name);
+        hash = 89 * hash + Objects.hashCode(this.status);
+        hash = 89 * hash + Objects.hashCode(this.date_created);
+        hash = 89 * hash + Objects.hashCode(this.author_id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BookCategoryResponse other = (BookCategoryResponse) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (this.status != other.status) {
+            return false;
+        }
+        if (!Objects.equals(this.date_created, other.date_created)) {
+            return false;
+        }
+        return Objects.equals(this.author_id, other.author_id);
+    }
+
+
+}
