e1eaba99471dc9f1101588e6f85bb68f0f1d5132
Muyinda Rogers <moverr@gmail.com>
2017-12-20 21:42:07 +0300

    books

---
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/books/entities/BookResponse.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.engine.api.v1.books.entities;
+
+import com.codemovers.scholar.engine.helper.enums.StatusEnum;
+import java.util.Objects;
+
+/**
+ *
+ * @author mover 12/20/2017
+ */
+public class BookResponse {
+
+    private Integer id;
+    private Integer category_id;
+    private Integer type_id;
+    private String name;
+    private String book_author;
+    private StatusEnum status;
+    private Integer author_id;
+
+    public BookResponse() {
+    }
+
+    public BookResponse(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Integer category_id) {
+        this.category_id = category_id;
+    }
+
+    public Integer getType_id() {
+        return type_id;
+    }
+
+    public void setType_id(Integer type_id) {
+        this.type_id = type_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBook_author() {
+        return book_author;
+    }
+
+    public void setBook_author(String book_author) {
+        this.book_author = book_author;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public Integer getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(Integer author_id) {
+        this.author_id = author_id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 61 * hash + Objects.hashCode(this.id);
+        hash = 61 * hash + Objects.hashCode(this.category_id);
+        hash = 61 * hash + Objects.hashCode(this.type_id);
+        hash = 61 * hash + Objects.hashCode(this.name);
+        hash = 61 * hash + Objects.hashCode(this.book_author);
+        hash = 61 * hash + Objects.hashCode(this.status);
+        hash = 61 * hash + Objects.hashCode(this.author_id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BookResponse other = (BookResponse) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.book_author, other.book_author)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.category_id, other.category_id)) {
+            return false;
+        }
+        if (!Objects.equals(this.type_id, other.type_id)) {
+            return false;
+        }
+        if (this.status != other.status) {
+            return false;
+        }
+        return Objects.equals(this.author_id, other.author_id);
+    }
+
+    @Override
+    public String toString() {
+        return "BookResponse{"
+                + "id=" + id
+                + ", category_id=" + category_id
+                + ", type_id=" + type_id
+                + ", name=" + name
+                + ", book_author=" + book_author
+                + ", status=" + status
+                + ", author_id=" + author_id
+                + "}";
+    }
+
+
+
+}
