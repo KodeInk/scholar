00169e2e37b092e5f9de1ffe979822eb46e63752
Muyinda Rogers <moverr@gmail.com>
2017-12-28 21:14:12 +0300

    Curriculum Detail Service

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/curriculum/details/CurriculumDetailEndpoint.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/curriculum/details/CurriculumDetailEndpoint.java
@@ -5,10 +5,71 @@
  */
 package com.codemovers.scholar.engine.api.v1.curriculum.details;
 
+import com.codemovers.scholar.engine.api.v1.abstracts.AbstractEndpoint;
+import com.codemovers.scholar.engine.api.v1.accounts.entities.AuthenticationResponse;
+import com.codemovers.scholar.engine.api.v1.classes.ClassEndpoint;
+import com.codemovers.scholar.engine.api.v1.classes.ClassService;
+import com.codemovers.scholar.engine.api.v1.curriculum.details.entities.CurriculumDetailResponse;
+import com.codemovers.scholar.engine.api.v1.curriculum.details.entities._CurriculumDetail;
+import com.codemovers.scholar.engine.api.v1.users.UserService;
+import com.codemovers.scholar.engine.db.entities.SchoolData;
+import java.util.Collection;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
+
 /**
  *
  * @author mover 12/28/2017
  */
-public class CurriculumDetailEndpoint {
+public class CurriculumDetailEndpoint extends AbstractEndpoint<_CurriculumDetail, CurriculumDetailResponse> {
+
+    private static final Logger LOG = Logger.getLogger(CurriculumDetailEndpoint.class.getName());
+    @Context
+    private ContainerRequestContext context;
+
+    private CurriculumDetailService service = null;
+    private AuthenticationResponse authentication = null;
+
+    /**
+     *
+     */
+    public CurriculumDetailEndpoint() {
+        service = new CurriculumDetailService();
+    }
+
+    /**
+     *
+     * @param schoolData
+     * @param authentication
+     * @throws Exception
+     */
+    @Override
+    public void validate(SchoolData schoolData, String authentication) throws Exception {
+        this.authentication = UserService.getInstance().validateAuthentication(schoolData, authentication);
+    }
+
+    @Override
+    public CurriculumDetailResponse create(_CurriculumDetail entity, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.create(entity, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CurriculumDetailResponse update(_CurriculumDetail entity, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.update(entity, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CurriculumDetailResponse archive(Integer id, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.archive(id, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<CurriculumDetailResponse> list(int start, int end, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.list(start, end, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
 
 }
--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/curriculum/details/CurriculumDetailService.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/curriculum/details/CurriculumDetailService.java
@@ -5,10 +5,37 @@
  */
 package com.codemovers.scholar.engine.api.v1.curriculum.details;
 
+import com.codemovers.scholar.engine.api.v1.abstracts.AbstractService;
+import com.codemovers.scholar.engine.api.v1.curriculum.CurriculumService;
+import com.codemovers.scholar.engine.api.v1.curriculum.details.entities.CurriculumDetailResponse;
+import com.codemovers.scholar.engine.api.v1.curriculum.details.entities._CurriculumDetail;
+import com.codemovers.scholar.engine.db.controllers.CurriculumDetailsJpaController;
+import com.codemovers.scholar.engine.db.controllers.CurriculumJpaController;
+import java.util.logging.Logger;
+
 /**
  *
  * @author mover 12/28/2017
  */
-public class CurriculumDetailService {
+public class CurriculumDetailService extends AbstractService<_CurriculumDetail, CurriculumDetailResponse> {
+
+    private static final Logger LOG = Logger.getLogger(CurriculumService.class.getName());
+
+    private final CurriculumDetailsJpaController controller;
+
+    private static CurriculumDetailService service = null;
+
+    public CurriculumDetailService() {
+        controller = CurriculumDetailsJpaController.getInstance();
+
+    }
+
+    public static CurriculumDetailService getInstance() {
+        if (service == null) {
+            service = new CurriculumDetailService();
+        }
+        return service;
+    }
+
 
 }
