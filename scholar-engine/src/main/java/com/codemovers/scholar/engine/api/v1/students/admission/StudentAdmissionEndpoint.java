bd1768512917f08c7f8811e9fbd57fb12831ada8
Muyinda Rogers <moverr@gmail.com>
2017-12-30 21:01:44 +0300

    StudentAdmission Endpoint

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/students/admission/StudentAdmissionEndpoint.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/students/admission/StudentAdmissionEndpoint.java
@@ -5,10 +5,60 @@
  */
 package com.codemovers.scholar.engine.api.v1.students.admission;
 
+import com.codemovers.scholar.engine.api.v1.abstracts.AbstractEndpoint;
+import com.codemovers.scholar.engine.api.v1.accounts.entities.AuthenticationResponse;
+import com.codemovers.scholar.engine.api.v1.students.admission.entities.StudentAdmissionResponse;
+import com.codemovers.scholar.engine.api.v1.students.admission.entities._StudentAdmission;
+import com.codemovers.scholar.engine.api.v1.users.UserService;
+import com.codemovers.scholar.engine.db.entities.SchoolData;
+import java.util.Collection;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
+
 /**
  *
- * @author Manny
+ * @author mover 12/30/2017
  */
-public class StudentAdmissionEndpoint {
+public class StudentAdmissionEndpoint extends AbstractEndpoint<_StudentAdmission, StudentAdmissionResponse> {
+
+    private static final Logger LOG = Logger.getLogger(StudentAdmissionEndpoint.class.getName());
+    @Context
+    private ContainerRequestContext context;
+    StudentAdmissionService service = null;
+    private AuthenticationResponse authentication = null;
+
+    public StudentAdmissionEndpoint() {
+        service = new StudentAdmissionService();
+    }
+
+    @Override
+    public void validate(SchoolData schoolData, String authentication) throws Exception {
+        this.authentication = UserService.getInstance().validateAuthentication(schoolData, authentication);
+    }
+
+    @Override
+    public StudentAdmissionResponse create(_StudentAdmission entity, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.create(entity, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudentAdmissionResponse update(_StudentAdmission entity, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.update(entity, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudentAdmissionResponse archive(Integer id, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.archive(id, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<StudentAdmissionResponse> list(int start, int end, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.list(start, end, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+
 
 }
