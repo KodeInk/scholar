adb23129bacc80bff8f097c12ccddb25527f2764
Muyinda Rogers <moverr@gmail.com>
2017-12-30 21:05:11 +0300

    Student Admissions Endpoint and Service

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/students/admission/StudentAdmissionService.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/students/admission/StudentAdmissionService.java
@@ -5,10 +5,64 @@
  */
 package com.codemovers.scholar.engine.api.v1.students.admission;
 
+import com.codemovers.scholar.engine.api.v1.abstracts.AbstractService;
+import com.codemovers.scholar.engine.api.v1.accounts.entities.AuthenticationResponse;
+import com.codemovers.scholar.engine.api.v1.streams.StreamsService;
+import com.codemovers.scholar.engine.api.v1.students.admission.entities.StudentAdmissionResponse;
+import com.codemovers.scholar.engine.api.v1.students.admission.entities._StudentAdmission;
+import com.codemovers.scholar.engine.db.controllers.StreamsJpaController;
+import com.codemovers.scholar.engine.db.controllers.StudentAdmissionJpaController;
+import com.codemovers.scholar.engine.db.entities.SchoolData;
+import java.util.List;
+import java.util.logging.Logger;
+
 /**
  *
- * @author Manny
+ * @author mover 12/30/2017
  */
-public class StudentAdmissionService {
+public class StudentAdmissionService extends AbstractService<_StudentAdmission, StudentAdmissionResponse> {
+
+    private static final Logger LOG = Logger.getLogger(StreamsService.class.getName());
+    private final StudentAdmissionJpaController controller;
+    private static StudentAdmissionService service = null;
+
+    public StudentAdmissionService() {
+        controller = StudentAdmissionJpaController.getInstance();
+    }
+
+    public static StudentAdmissionService getInstance() {
+        if (service == null) {
+            service = new StudentAdmissionService();
+        }
+        return service;
+    }
+
+    @Override
+    public StudentAdmissionResponse create(SchoolData data, _StudentAdmission entity, AuthenticationResponse authentication) throws Exception {
+        return super.create(data, entity, authentication); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudentAdmissionResponse update(SchoolData data, _StudentAdmission entity, AuthenticationResponse authentication) throws Exception {
+        return super.update(data, entity, authentication); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudentAdmissionResponse archive(SchoolData data, Integer id, AuthenticationResponse authentication) throws Exception {
+        return super.archive(data, id, authentication); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudentAdmissionResponse getById(SchoolData data, Integer Id) throws Exception {
+        return super.getById(data, Id); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<StudentAdmissionResponse> list(SchoolData data, Integer ofset, Integer limit, AuthenticationResponse authentication) throws Exception {
+        return super.list(data, ofset, limit, authentication); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+
 
 }
