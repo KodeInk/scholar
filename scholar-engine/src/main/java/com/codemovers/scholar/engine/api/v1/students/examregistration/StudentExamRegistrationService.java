8128ed430f16736dd616ce11ade43c2f6b32f673
Muyinda Rogers <moverr@gmail.com>
2018-01-02 08:34:53 +0300

    832

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/students/examregistration/StudentExamRegistrationService.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/students/examregistration/StudentExamRegistrationService.java
@@ -7,10 +7,12 @@ package com.codemovers.scholar.engine.api.v1.students.examregistration;
 
 import com.codemovers.scholar.engine.api.v1.abstracts.AbstractService;
 import com.codemovers.scholar.engine.api.v1.accounts.entities.AuthenticationResponse;
+import static com.codemovers.scholar.engine.api.v1.students.admission.StudentAdmissionServiceInterface.ADMIT_STUDENT_PERMISSION;
 import com.codemovers.scholar.engine.api.v1.students.examregistration.entities.StudentExamRegistrationResponse;
 import com.codemovers.scholar.engine.api.v1.students.examregistration.entities._StudentExamRegistration;
 import com.codemovers.scholar.engine.db.controllers.StudentExamRegistrationJpaController;
 import com.codemovers.scholar.engine.db.entities.SchoolData;
+import static com.codemovers.scholar.engine.helper.Utilities.check_access;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -23,6 +25,12 @@ public class StudentExamRegistrationService extends AbstractService<_StudentExam
     private static final Logger LOG = Logger.getLogger(StudentExamRegistrationService.class.getName());
     private final StudentExamRegistrationJpaController controller;
     private static StudentExamRegistrationService service = null;
+
+    final String[] CREATE_EXAMREGISTRATION_PERMISSION = new String[]{"ALL_FUNCTIONS", "CREATE_EXAMREGISTRATION"};
+    final String[] UPDATE_EXAMREGISTRATION_PERMISSION = new String[]{"ALL_FUNCTIONS", "UPDATE_EXAMREGISTRATION"};
+    final String[] LIST_EXAMREGISTRATION_PERMISSION = new String[]{"ALL_FUNCTIONS", "LIST_EXAMREGISTRATION"};
+    final String[] ARCHIVE_EXAMREGISTRATION_PERMISSION = new String[]{"ALL_FUNCTIONS", "ARCHIVE_EXAMREGISTRATION"};
+
 
     public StudentExamRegistrationService() {
         controller = StudentExamRegistrationJpaController.getInstance();
@@ -37,26 +45,32 @@ public class StudentExamRegistrationService extends AbstractService<_StudentExam
 
     @Override
     public StudentExamRegistrationResponse create(SchoolData data, _StudentExamRegistration entity, AuthenticationResponse authentication) throws Exception {
+        check_access(CREATE_EXAMREGISTRATION_PERMISSION);
+
         return super.create(data, entity, authentication); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public StudentExamRegistrationResponse update(SchoolData data, _StudentExamRegistration entity, AuthenticationResponse authentication) throws Exception {
+        check_access(UPDATE_EXAMREGISTRATION_PERMISSION);
         return super.update(data, entity, authentication); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public StudentExamRegistrationResponse archive(SchoolData data, Integer id, AuthenticationResponse authentication) throws Exception {
+        check_access(ARCHIVE_EXAMREGISTRATION_PERMISSION);
         return super.archive(data, id, authentication); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public List<StudentExamRegistrationResponse> list(SchoolData data, Integer ofset, Integer limit, AuthenticationResponse authentication) throws Exception {
+        check_access(LIST_EXAMREGISTRATION_PERMISSION);
         return super.list(data, ofset, limit, authentication); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public StudentExamRegistrationResponse getById(SchoolData data, Integer Id) throws Exception {
+        check_access(LIST_EXAMREGISTRATION_PERMISSION);
         return super.getById(data, Id); //To change body of generated methods, choose Tools | Templates.
     }
 
