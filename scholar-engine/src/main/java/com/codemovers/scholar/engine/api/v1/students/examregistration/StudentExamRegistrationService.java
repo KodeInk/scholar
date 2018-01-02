da1ac3a40cbd6e5a502e52f88deabbbdd3f86aa9
Muyinda Rogers <moverr@gmail.com>
2018-01-02 08:29:50 +0300

    32323

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/students/examregistration/StudentExamRegistrationService.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/students/examregistration/StudentExamRegistrationService.java
@@ -6,11 +6,12 @@
 package com.codemovers.scholar.engine.api.v1.students.examregistration;
 
 import com.codemovers.scholar.engine.api.v1.abstracts.AbstractService;
-import com.codemovers.scholar.engine.api.v1.students.admission.StudentAdmissionService;
+import com.codemovers.scholar.engine.api.v1.accounts.entities.AuthenticationResponse;
 import com.codemovers.scholar.engine.api.v1.students.examregistration.entities.StudentExamRegistrationResponse;
 import com.codemovers.scholar.engine.api.v1.students.examregistration.entities._StudentExamRegistration;
-import com.codemovers.scholar.engine.db.controllers.StudentAdmissionJpaController;
 import com.codemovers.scholar.engine.db.controllers.StudentExamRegistrationJpaController;
+import com.codemovers.scholar.engine.db.entities.SchoolData;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -32,6 +33,31 @@ public class StudentExamRegistrationService extends AbstractService<_StudentExam
             service = new StudentExamRegistrationService();
         }
         return service;
+    }
+
+    @Override
+    public StudentExamRegistrationResponse create(SchoolData data, _StudentExamRegistration entity, AuthenticationResponse authentication) throws Exception {
+        return super.create(data, entity, authentication); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudentExamRegistrationResponse update(SchoolData data, _StudentExamRegistration entity, AuthenticationResponse authentication) throws Exception {
+        return super.update(data, entity, authentication); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudentExamRegistrationResponse archive(SchoolData data, Integer id, AuthenticationResponse authentication) throws Exception {
+        return super.archive(data, id, authentication); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<StudentExamRegistrationResponse> list(SchoolData data, Integer ofset, Integer limit, AuthenticationResponse authentication) throws Exception {
+        return super.list(data, ofset, limit, authentication); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudentExamRegistrationResponse getById(SchoolData data, Integer Id) throws Exception {
+        return super.getById(data, Id); //To change body of generated methods, choose Tools | Templates.
     }
 
 
