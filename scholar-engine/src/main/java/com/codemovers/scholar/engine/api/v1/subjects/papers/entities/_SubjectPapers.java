4eaa3711578e5fe19676b849f77cd361a3cb3d39
Muyinda Rogers <moverr@gmail.com>
2018-01-03 10:23:55 +0300

    Entities

---
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/subjects/papers/entities/_SubjectPapers.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.engine.api.v1.subjects.papers.entities;
+
+import com.codemovers.scholar.engine.annotation.Mandatory;
+import com.codemovers.scholar.engine.api.v1.abstracts.AbstractEntity;
+import static com.codemovers.scholar.engine.helper.Utilities.validateMandatoryFields;
+import com.codemovers.scholar.engine.helper.enums.StatusEnum;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author mover 1/4/2018
+ */
+public class _SubjectPapers extends AbstractEntity {
+
+    private Integer id;
+    private @Mandatory
+    Integer subject_id;
+    private @Mandatory
+    String name;
+    private @Mandatory
+    String code;
+    private StatusEnum status;
+    private Integer author_id;
+    private Date date_created;
+
+    public _SubjectPapers() {
+    }
+
+    public _SubjectPapers(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSubject_id() {
+        return subject_id;
+    }
+
+    public void setSubject_id(Integer subject_id) {
+        this.subject_id = subject_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public Integer getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(Integer author_id) {
+        this.author_id = author_id;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        hash = 59 * hash + Objects.hashCode(this.subject_id);
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + Objects.hashCode(this.code);
+        hash = 59 * hash + Objects.hashCode(this.status);
+        hash = 59 * hash + Objects.hashCode(this.author_id);
+        hash = 59 * hash + Objects.hashCode(this.date_created);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final _SubjectPapers other = (_SubjectPapers) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.code, other.code)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.subject_id, other.subject_id)) {
+            return false;
+        }
+        if (this.status != other.status) {
+            return false;
+        }
+        if (!Objects.equals(this.author_id, other.author_id)) {
+            return false;
+        }
+        return Objects.equals(this.date_created, other.date_created);
+    }
+
+    @Override
+    public void validate() {
+        validateMandatoryFields(this.getClass(), this);
+    }
+
+    @Override
+    public String toString() {
+        return "_SubjectPapers{"
+                + "id=" + id
+                + ", subject_id=" + subject_id
+                + ", name=" + name
+                + ", code=" + code
+                + ", status=" + status
+                + ", author_id=" + author_id
+                + ", date_created=" + date_created
+                + "}";
+    }
+
+
+
+}
