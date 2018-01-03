dc1ce8130a70086f6f916866ac5827ec78d464e9
Muyinda Rogers <moverr@gmail.com>
2018-01-03 11:22:14 +0300

    Subject paper Endpoint

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/subjects/papers/SubjectPapersEndpoint.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/subjects/papers/SubjectPapersEndpoint.java
@@ -5,10 +5,37 @@
  */
 package com.codemovers.scholar.engine.api.v1.subjects.papers;
 
+import com.codemovers.scholar.engine.api.v1.abstracts.AbstractEndpoint;
+import com.codemovers.scholar.engine.api.v1.subjects.SubjectEndpoint;
+import com.codemovers.scholar.engine.api.v1.subjects.SubjectService;
+import com.codemovers.scholar.engine.api.v1.subjects.papers.entities.SubjectPapersResponse;
+import com.codemovers.scholar.engine.api.v1.subjects.papers.entities._SubjectPapers;
+import com.codemovers.scholar.engine.db.entities.SchoolData;
+import java.util.logging.Logger;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
+
 /**
  *
  * @author mover 1/3/2018
  */
-public class SubjectPapersEndpoint {
+public class SubjectPapersEndpoint extends AbstractEndpoint<_SubjectPapers, SubjectPapersResponse> {
+
+    private static final Logger LOG = Logger.getLogger(SubjectPapersEndpoint.class.getName());
+    @Context
+    private ContainerRequestContext context;
+
+    SubjectPapersService service = null;
+
+    public SubjectPapersEndpoint() {
+        service = new SubjectPapersService();
+    }
+
+    @Override
+    public void validateAuthentication(SchoolData schoolData, String authentication) {
+        super.validateAuthentication(schoolData, authentication); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
 
 }
