7facd4304b6d276c769d08d607e83c46a6f642e2
Muyinda Rogers <moverr@gmail.com>
2018-01-03 11:02:12 +0300

    Subject Response

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/subjects/papers/entities/SubjectPapersResponse.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/subjects/papers/entities/SubjectPapersResponse.java
@@ -5,10 +5,146 @@
  */
 package com.codemovers.scholar.engine.api.v1.subjects.papers.entities;
 
+import com.codemovers.scholar.engine.api.v1.subjects.entities.SubjectResponse;
+import java.util.Date;
+import java.util.Objects;
+
 /**
  *
- * @author Manny
+ * @author mover 1/3/2018
  */
 public class SubjectPapersResponse {
+
+    private Integer id;
+    private SubjectResponse subject;
+    private String name;
+    private String code;
+    private String status;
+    private String author;
+    private Date date_created;
+
+    public SubjectPapersResponse() {
+    }
+
+    public SubjectPapersResponse(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public SubjectResponse getSubject() {
+        return subject;
+    }
+
+    public void setSubject(SubjectResponse subject) {
+        this.subject = subject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 11 * hash + Objects.hashCode(this.id);
+        hash = 11 * hash + Objects.hashCode(this.subject);
+        hash = 11 * hash + Objects.hashCode(this.name);
+        hash = 11 * hash + Objects.hashCode(this.code);
+        hash = 11 * hash + Objects.hashCode(this.status);
+        hash = 11 * hash + Objects.hashCode(this.author);
+        hash = 11 * hash + Objects.hashCode(this.date_created);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SubjectPapersResponse other = (SubjectPapersResponse) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.code, other.code)) {
+            return false;
+        }
+        if (!Objects.equals(this.status, other.status)) {
+            return false;
+        }
+        if (!Objects.equals(this.author, other.author)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.subject, other.subject)) {
+            return false;
+        }
+        return Objects.equals(this.date_created, other.date_created);
+    }
+
+    @Override
+    public String toString() {
+        return "SubjectPapersResponse{"
+                + "id=" + id
+                + ", subject=" + subject
+                + ", name=" + name
+                + ", code=" + code
+                + ", status=" + status
+                + ", author=" + author
+                + ", date_created=" + date_created
+                + "}";
+    }
+
+
 
 }
