886c54096cbdabe6a0b3927d880945f4f463f757
Muyinda Rogers <moverr@gmail.com>
2018-03-06 07:52:03 +0300

    Init Roles Endpoint

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/roles/RolesEndpoint.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/roles/RolesEndpoint.java
@@ -12,7 +12,9 @@ import com.codemovers.scholar.engine.api.v1.roles.entities._Role;
 import com.codemovers.scholar.engine.api.v1.users.UserService;
 import com.codemovers.scholar.engine.api.v1.users.UserServiceInterface;
 import com.codemovers.scholar.engine.db.entities.SchoolData;
+import java.util.List;
 import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Path;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
@@ -38,4 +40,25 @@ public class RolesEndpoint extends AbstractEndpoint<_Role, RoleResponse> {
     public void validate(SchoolData schoolData, String authentication) throws Exception {
         this.authentication = UserService.getInstance().validateAuthentication(schoolData, authentication);
     }
+
+    @Override
+    public RoleResponse create(_Role entity, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.create(entity, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public RoleResponse update(_Role entity, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.update(entity, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<RoleResponse> list(int start, int end, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.list(start, end, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public RoleResponse archive(Integer id, String authentication, HttpServletRequest httpRequest) throws Exception {
+        return super.archive(id, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
