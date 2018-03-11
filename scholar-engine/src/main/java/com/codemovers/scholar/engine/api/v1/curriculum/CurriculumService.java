b3e35cdeae5de589d647ef9738cdbad154d7c83b
Muyinda Rogers <moverr@gmail.com>
2018-03-11 10:29:41 +0300

    Debuging Curriculum Endpoint :

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/curriculum/CurriculumService.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/curriculum/CurriculumService.java
@@ -119,10 +119,10 @@ public class CurriculumService extends AbstractService<_Curriculum, CurriculumRe
     }
 
     @Override
-    public List<CurriculumResponse> list(SchoolData data, Integer ofset, Integer limit) throws Exception {
-        check_access(LIST_CURRICULUM_PERMISSION);
+    public List<CurriculumResponse> list(SchoolData data, Integer ofset, Integer limit, AuthenticationResponse authenticationResponse) throws Exception {
+        // check_access(LIST_CURRICULUM_PERMISSION);
 
-        List<Curriculum> list = controller.findCurriculumEntities(ofset, limit, data);
+        List<Curriculum> list = controller.findCurriculumEntities(limit, ofset, data);
         List<CurriculumResponse> responses = new ArrayList<>();
         if (list != null) {
             list.forEach((_class) -> {
