90622e15b85b12a3d9faf65c4c8de98bb51ccc20
Muyinda Rogers <moverr@gmail.com>
2018-03-16 00:14:34 +0300

    Debugging  :

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/departments/DepartmentsEdnpoint.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/departments/DepartmentsEdnpoint.java
@@ -5,10 +5,97 @@
  */
 package com.codemovers.scholar.engine.api.v1.departments;
 
+import com.codemovers.scholar.engine.api.v1.abstracts.AbstractEndpoint;
+import com.codemovers.scholar.engine.api.v1.accounts.entities.AuthenticationResponse;
+import com.codemovers.scholar.engine.api.v1.curriculum.entities._Curriculum;
+import com.codemovers.scholar.engine.api.v1.departments.entities.DepartmentResponse;
+import com.codemovers.scholar.engine.api.v1.departments.entities._Department;
+import com.codemovers.scholar.engine.api.v1.users.UserService;
+import com.codemovers.scholar.engine.db.entities.SchoolData;
+import static com.codemovers.scholar.engine.helper.Utilities.tenantdata;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
 /**
  *
  * @author mover 3/15/2018
  */
-public class DepartmentsEdnpoint {
+public class DepartmentsEdnpoint extends AbstractEndpoint<_Department, DepartmentResponse> {
+
+    private static final Logger LOG = Logger.getLogger(DepartmentsEdnpoint.class.getName());
+    @Context
+    private ContainerRequestContext context;
+    private DepartmentsService service = null;
+    private AuthenticationResponse authentication = null;
+
+    public DepartmentsEdnpoint() {
+        service = new DepartmentsService();
+    }
+
+    @Override
+    public void validate(SchoolData schoolData, String authentication) throws Exception {
+        this.authentication = UserService.getInstance().validateAuthentication(schoolData, authentication);
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Override
+    public DepartmentResponse create(_Curriculum entity,
+            @HeaderParam("authentication") String authentication,
+            @Context HttpServletRequest httpRequest) throws Exception {
+        validate(tenantdata, authentication);
+        return service.create(tenantdata, entity, this.authentication);
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Override
+    public DepartmentResponse update(_Department entity,
+            @HeaderParam("authentication") String authentication,
+            @Context HttpServletRequest httpRequest) throws Exception {
+        validate(tenantdata, authentication);
+        return service.update(tenantdata, entity, this.authentication);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Override
+    public List<DepartmentResponse> list(
+            @DefaultValue("0") @QueryParam("offset") int offset,
+            @DefaultValue("50") @QueryParam("limit") int limit,
+            @HeaderParam("authentication") String authentication,
+            @Context HttpServletRequest httpRequest
+    ) throws Exception {
+        validate(tenantdata, authentication);
+        return service.list(tenantdata, offset, limit, this.authentication);
+    }
+
+    @POST
+    @Path("/archive/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Override
+    public DepartmentResponse archive(Integer id,
+            @HeaderParam("authentication") String authentication,
+            @Context HttpServletRequest httpRequest) throws Exception {
+        validate(tenantdata, authentication);
+        return service.archive(tenantdata, id, this.authentication);
+    }
 
 }
