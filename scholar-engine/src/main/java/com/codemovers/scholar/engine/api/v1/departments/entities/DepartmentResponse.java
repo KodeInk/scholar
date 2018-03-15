f189df90eafd68eca1db3c91d7f3d4aac64e6996
Muyinda Rogers <moverr@gmail.com>
2018-03-16 00:10:00 +0300

    Debugigng  ;

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/departments/entities/DepartmentResponse.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/departments/entities/DepartmentResponse.java
@@ -5,8 +5,6 @@
  */
 package com.codemovers.scholar.engine.api.v1.departments.entities;
 
-import com.codemovers.scholar.engine.annotation.Mandatory;
-import com.codemovers.scholar.engine.helper.enums.StatusEnum;
 import java.util.Objects;
 
 /**
