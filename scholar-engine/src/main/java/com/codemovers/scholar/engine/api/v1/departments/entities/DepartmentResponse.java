e4bddb8e22c1725373b8d3c9688d8e6c3a78cdde
Muyinda Rogers <moverr@gmail.com>
2018-03-16 00:02:48 +0300

    Request Response Department :

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/departments/entities/DepartmentResponse.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/departments/entities/DepartmentResponse.java
@@ -5,10 +5,126 @@
  */
 package com.codemovers.scholar.engine.api.v1.departments.entities;
 
+import com.codemovers.scholar.engine.annotation.Mandatory;
+import com.codemovers.scholar.engine.helper.enums.StatusEnum;
+import java.util.Objects;
+
 /**
  *
  * @author mover 3/15/2018
  */
 public class DepartmentResponse {
+
+    private Integer id;
+    private String name;
+    private Boolean isSystem;
+    private String status;
+    private Long date_created;
+    private String author;
+
+    public DepartmentResponse() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getIsSystem() {
+        return isSystem;
+    }
+
+    public void setIsSystem(Boolean isSystem) {
+        this.isSystem = isSystem;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Long date_created) {
+        this.date_created = date_created;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        hash = 97 * hash + Objects.hashCode(this.name);
+        hash = 97 * hash + Objects.hashCode(this.isSystem);
+        hash = 97 * hash + Objects.hashCode(this.status);
+        hash = 97 * hash + Objects.hashCode(this.date_created);
+        hash = 97 * hash + Objects.hashCode(this.author);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final DepartmentResponse other = (DepartmentResponse) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.status, other.status)) {
+            return false;
+        }
+        if (!Objects.equals(this.author, other.author)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.isSystem, other.isSystem)) {
+            return false;
+        }
+        return Objects.equals(this.date_created, other.date_created);
+    }
+
+    @Override
+    public String toString() {
+        return "DepartmentResponse{"
+                + "id=" + id
+                + ", name=" + name
+                + ", isSystem=" + isSystem
+                + ", status=" + status
+                + ", date_created=" + date_created
+                + ", author=" + author
+                + '}';
+    }
 
 }
