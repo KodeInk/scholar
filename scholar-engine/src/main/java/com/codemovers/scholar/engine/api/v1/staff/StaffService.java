823e8ba0aa0accfcf92f0a89c6673b0d9457c329
Muyinda Rogers <moverr@gmail.com>
2018-03-16 20:52:48 +0300

    StaffEdnpoint ;

---
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/staff/StaffService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.engine.api.v1.staff;
+
+/**
+ *
+ * @author mover 3/16/2018
+ */
+public class StaffService {
+
+}
