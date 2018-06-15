734d43e5858f4f5a4fc2cdd11dcf122308cb584d
Muyinda Rogers <moverr@gmail.com>
2018-06-15 13:46:12 +0300

    Admissions Endpoint :

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/admissions/AdmissionEndpoint.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/admissions/AdmissionEndpoint.java
@@ -6,56 +6,94 @@
 package com.codemovers.scholar.engine.api.v1.admissions;
 
 import com.codemovers.scholar.engine.api.v1.abstracts.AbstractEndpoint;
+import com.codemovers.scholar.engine.api.v1.accounts.entities.AuthenticationResponse;
 import com.codemovers.scholar.engine.api.v1.admissions.entities.AdmissionResponse;
 import com.codemovers.scholar.engine.api.v1.admissions.entities._Admission;
+import com.codemovers.scholar.engine.api.v1.users.UserService;
 import com.codemovers.scholar.engine.db.entities.SchoolData;
-import java.util.Collection;
+import static com.codemovers.scholar.engine.helper.Utilities.tenantdata;
 import java.util.List;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author mover 12/20/2017
  */
+@Path("/")
 public class AdmissionEndpoint extends AbstractEndpoint<_Admission, AdmissionResponse> {
 
     private static final Logger LOG = Logger.getLogger(AdmissionEndpoint.class.getName());
     @Context
     private ContainerRequestContext context;
-
     AdmissionService service = null;
+    private AuthenticationResponse authentication = null;
 
     public AdmissionEndpoint() {
         service = new AdmissionService();
     }
 
+    /**
+     *
+     * @param schoolData
+     * @param authentication
+     * @throws Exception
+     */
     @Override
-    public void validateAuthentication(SchoolData schoolData, String authentication) {
-        super.validateAuthentication(schoolData, authentication); //To change body of generated methods, choose Tools | Templates.
+    public void validate(SchoolData schoolData, String authentication) throws Exception {
+        this.authentication = UserService.getInstance().validateAuthentication(schoolData, authentication);
     }
 
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Override
     public AdmissionResponse create(_Admission entity, String authentication, HttpServletRequest httpRequest) throws Exception {
-        return super.create(entity, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+        validate(tenantdata, authentication);
+        return service.create(tenantdata, entity, this.authentication);
     }
 
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Override
     public AdmissionResponse update(_Admission entity, String authentication, HttpServletRequest httpRequest) throws Exception {
-        return super.update(entity, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+        validate(tenantdata, authentication);
+        return service.update(tenantdata, entity, this.authentication);
     }
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Override
-    public List<AdmissionResponse> list(int start, int end, String authentication, HttpServletRequest httpRequest) throws Exception {
-        return super.list(start, end, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    public List<AdmissionResponse> list(
+            @DefaultValue("0") @QueryParam("offset") int offset,
+            @DefaultValue("50") @QueryParam("limit") int limit,
+            @HeaderParam("authentication") String authentication, @Context HttpServletRequest httpRequest) throws Exception {
+        validate(tenantdata, authentication);
+        return service.list(tenantdata, offset, limit, this.authentication);
     }
 
+    @POST
+    @Path("/archive/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Override
     public AdmissionResponse archive(Integer id, String authentication, HttpServletRequest httpRequest) throws Exception {
-        return super.archive(id, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+        validate(tenantdata, authentication);
+        return service.archive(tenantdata, id, this.authentication);
     }
 
 }
