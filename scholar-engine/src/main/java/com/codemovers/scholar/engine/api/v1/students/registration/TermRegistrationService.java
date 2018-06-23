5e4ea10f7c6a4dfc39fb088af4f878fe104953d1
Muyinda Rogers <moverr@gmail.com>
2018-06-23 14:57:22 +0300

    validate and Populate entity registration :

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/students/registration/TermRegistrationService.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/students/registration/TermRegistrationService.java
@@ -18,6 +18,8 @@ import com.codemovers.scholar.engine.db.entities.SchoolData;
 import com.codemovers.scholar.engine.db.entities.StudentAdmission;
 import com.codemovers.scholar.engine.db.entities.StudentTermRegistration;
 import com.codemovers.scholar.engine.db.entities.Terms;
+import com.codemovers.scholar.engine.db.entities.Users;
+import com.codemovers.scholar.engine.helper.enums.StatusEnum;
 import com.codemovers.scholar.engine.helper.exceptions.BadRequestException;
 import java.util.Date;
 import java.util.List;
@@ -28,85 +30,98 @@ import java.util.logging.Logger;
  * @author mover
  */
 public class TermRegistrationService extends AbstractService<_TermRegistration, TermRegistrationResponse> {
-
+    
     private static final Logger LOG = Logger.getLogger(AdmissionService.class.getName());
     private final StudentTermRegistrationJpaController controller;
     private static TermRegistrationService instance = null;
-
+    
     public TermRegistrationService() {
         controller = StudentTermRegistrationJpaController.getInstance();
     }
-
+    
     public static TermRegistrationService getInstance() {
         if (instance == null) {
             instance = new TermRegistrationService();
         }
-
+        
         return instance;
-
+        
     }
-
+    
     @Override
     public TermRegistrationResponse create(SchoolData data, _TermRegistration entity, AuthenticationResponse authentication) throws Exception {
-       //todo: validate
-       entity.validate();
-       
-    //todo: check  addmission class
-        Classes admissionClass = ClassService.getInstance().getClass(entity.getClass_id(), data);
-        if (admissionClass == null) {
-            throw new BadRequestException("Class does not exist in the system");
-        }
-        //todo: validate admission term 
-        Terms admissionTerm = TermService.getInstance().getTerm(entity.getTerm_id(), data);
+        //todo: validate
+        entity.validate();
+        entity.setDate_registered(entity.getDate_registered() != null ? entity.getDate_registered() : new Date().getTime());
+        entity.setAuthor_id(authentication.getId());
+        entity.setStatus(StatusEnum.ACTIVE.name());
+        
+        Classes registrationClass = validateClassOfRegistration(entity, data);
+        Terms registrationTerm = validateTermOfRegistration(entity, data);
+        
+        StudentAdmission admission = validateStudentAdmission(data, entity, authentication);
 
-        if (admissionTerm == null) {
-            throw new BadRequestException("Term  does not exist in the system");
-        }
-
-        //todo: get Admission By Id 
-        StudentAdmission admission = AdmissionService.getInstance().getAdmission(data, entity.getAddmision_id(), authentication);
-         if (admission == null) {
-            throw new BadRequestException("Student Admission   does not exist in the system");
-        }
-        
-         
-        
-       
-       //todo: populate entity
-       StudentTermRegistration termRegistration = new StudentTermRegistration();
-       termRegistration.setDateCreated(new Date());
-//       termRegistration.setRegistration_Class(Registration_Class);
-       
-       
-       //todo: check if student already registered for this term
-       //todo: create entity
-       //todo: populate response 
-        
+        //todo: populate entity
+        StudentTermRegistration termRegistration = new StudentTermRegistration();
+        termRegistration.setDateCreated(new Date());
+        termRegistration.setRegistration_Class(registrationClass);
+        termRegistration.setRegistration_term(registrationTerm);
+        termRegistration.setStudent_Admission(admission);
+        termRegistration.setDateRegistered(new Date(entity.getDate_registered()));
+        termRegistration.setDateCreated(new Date());
+        termRegistration.setStatus(entity.getStatus());
+        termRegistration.setAuthor(new Users(entity.getAuthor_id().longValue()));
+        //todo: check if student already registered for this term
+        //todo: create entity
+        //todo: populate response 
         return super.create(data, entity, authentication); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
+    public StudentAdmission validateStudentAdmission(SchoolData data, _TermRegistration entity, AuthenticationResponse authentication) throws Exception, BadRequestException {
+        //todo: get Admission By Id
+        StudentAdmission admission = AdmissionService.getInstance().getAdmission(data, entity.getAddmision_id(), authentication);
+        if (admission == null) {
+            throw new BadRequestException("Student Admission   does not exist in the system");
+        }
+        return admission;
+    }
+    
+    public Terms validateTermOfRegistration(_TermRegistration entity, SchoolData data) throws BadRequestException {
+        //todo: validate admission term
+        Terms registrationTerm = TermService.getInstance().getTerm(entity.getTerm_id(), data);
+        if (registrationTerm == null) {
+            throw new BadRequestException("Term  does not exist in the system");
+        }
+        return registrationTerm;
+    }
+    
+    public Classes validateClassOfRegistration(_TermRegistration entity, SchoolData data) throws BadRequestException {
+        //todo: check  addmission class
+        Classes registrationClass = ClassService.getInstance().getClass(entity.getClass_id(), data);
+        if (registrationClass == null) {
+            throw new BadRequestException("Class does not exist in the system");
+        }
+        return registrationClass;
+    }
+    
     @Override
     public TermRegistrationResponse update(SchoolData data, _TermRegistration entity, AuthenticationResponse authentication) throws Exception {
         return super.update(data, entity, authentication); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public TermRegistrationResponse getById(SchoolData data, Integer Id, AuthenticationResponse authentication) throws Exception {
         return super.getById(data, Id, authentication); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public List<TermRegistrationResponse> list(SchoolData data, Integer ofset, Integer limit, AuthenticationResponse authentication) throws Exception {
         return super.list(data, ofset, limit, authentication); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public TermRegistrationResponse archive(SchoolData data, Integer id, AuthenticationResponse authentication) throws Exception {
         return super.archive(data, id, authentication); //To change body of generated methods, choose Tools | Templates.
     }
-
-    
-    
-    
     
 }
