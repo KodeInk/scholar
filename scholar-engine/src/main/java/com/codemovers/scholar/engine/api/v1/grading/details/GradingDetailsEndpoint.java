7cc168ec36656f4f8aaa9dd66caa47f8f4a50cc0
Muyinda Rogers <moverr@gmail.com>
2018-08-19 17:18:31 +0300

    Debugging :

--- scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/grading/details/GradingDetailsEndpoint.java
+++ scholar-engine/src/main/java/com/codemovers/scholar/engine/api/v1/grading/details/GradingDetailsEndpoint.java
@@ -16,9 +16,15 @@ import java.util.List;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -53,27 +59,51 @@ public class GradingDetailsEndpoint extends AbstractEndpoint<GradingDetail, Grad
             @HeaderParam("authentication") String authentication,
             @Context HttpServletRequest httpRequest
     ) throws Exception {
-          validate(tenantdata, authentication);
-       return  service.create(tenantdata, entity, this.authentication);
+        validate(tenantdata, authentication);
+        return service.create(tenantdata, entity, this.authentication);
     }
 
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Override
-    public GradingDetailResponse update(GradingDetail entity, String authentication, HttpServletRequest httpRequest) throws Exception {
-        return super.update(entity, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    public GradingDetailResponse update(
+            GradingDetail entity,
+            @HeaderParam("authentication") String authentication,
+            @Context HttpServletRequest httpRequest) throws Exception {
+        validate(tenantdata, authentication);
+        return service.update(tenantdata, entity, this.authentication);
     }
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Override
-    public List<GradingDetailResponse> list(int start, int end, String authentication, HttpServletRequest httpRequest) throws Exception {
-        return super.list(start, end, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
+    public List<GradingDetailResponse> list(
+            @DefaultValue("0") @QueryParam("offset") int offset,
+            @DefaultValue("50") @QueryParam("limit") int limit,
+            @HeaderParam("authentication") String authentication,
+            @Context HttpServletRequest httpRequest
+    ) throws Exception {
+        validate(tenantdata, authentication);
+        return service.list(tenantdata, offset, limit, this.authentication);
     }
 
+    @POST
+    @Path("/archive/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Override
-    public GradingDetailResponse get(Integer id, String authentication, HttpServletRequest httpRequest) throws Exception {
+    public GradingDetailResponse archive(
+            @PathParam("id") Integer id,
+            @HeaderParam("authentication") String authentication,
+            @Context HttpServletRequest httpRequest
+    ) throws Exception {
         return super.get(id, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public GradingDetailResponse archive(Integer id, String authentication, HttpServletRequest httpRequest) throws Exception {
+    public GradingDetailResponse get(Integer id, String authentication, HttpServletRequest httpRequest) throws Exception {
         return super.archive(id, authentication, httpRequest); //To change body of generated methods, choose Tools | Templates.
     }
 
